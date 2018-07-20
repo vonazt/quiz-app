c56bcd51438ba4e00ee151cccbd877d0df11bf03
Richard Tzanov <richardtzanov@gmail.com>
2018-07-20 19:12:55 +0100

    made initial axios request to trivia api

--- src/app.js
+++ src/app.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 class App extends React.Component {
+
+  componentDidMount = () => {
+
+    axios({
+      method: 'GET',
+      url: 'https://opentdb.com/api.php?amount=10&category=10&difficulty=medium&type=multiple'
+    })
+      .then(res => console.log(res.data));
+  }
+
   render() {
     return(
       <h1>Up and running</h1>
